cadb318ed33ae7011f758722536f5b9583248500
Pabloarraiza <pabloarraizag@gmail.com>
2025-11-28 19:00:33 +0100

    login/registro HTML creados

--- registro.php
+++ registro.php
@@ -1,20 +1,21 @@
 <?php
 
-    include "/includes/cabeceraproy.php";
+    include "/includes/cabecera.php";
 
-    echo "<h1>Crea tu cuenta: </h1>
-            <form action='gestion_registro.php' method='post'>
+?>
+    <h1>Crea tu cuenta: </h1>
+            <form action='index.php' method='post'>
                 <label for='usuario'>Nombre de usuario</label><br>
                     <input type='text' name='username'><br>
                 <label for='correo'>Correo electrónico</label> <br>
                     <input type='text' name='mail'><br>
                 <label for='passwd'>Contraseña</label> <br>
-                    <input type='password'><br>
+                    <input type='password' name='passwd'><br>
                 <label for='passwd2'>Confirmar contraseña</label> <br>
                     <input type='password' name='passwd2'><br>
                 <input type='submit' value='Crear cuenta'>
-            </form>";
+            </form>
 
-    include "/includes/pieproy.php";
-
+<?php            
+    include "/includes/pie.php";
 ?>
